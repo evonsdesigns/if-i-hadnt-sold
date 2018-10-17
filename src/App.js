176aed6a7bc0d3e5f653f53474200ce3eb2f25a6
Joseph Evans <evonsdesigns@gmail.com>
2018-10-16 19:02:11 -0500

    Fresh start
    with react-create-app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo"/>
+          <p>
+            Your present circumstances don't determine where you can go;
+          </p>
+          <p>
+            they merely determine where you start.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer">
+            Dont click here
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
