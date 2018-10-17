aa27b99c2d711dc832ee443bb54bd0d1b4da269f
Joseph Evans <evonsdesigns@gmail.com>
2018-10-16 19:26:27 -0500

    Add node-sass, structure app

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import logo from '../logo.svg';
+import '../sass/main.scss';
 
 class App extends Component {
   render() {
